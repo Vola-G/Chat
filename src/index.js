20ef291f62e522d0e3e1f73e9b370fd7475d5e69
Vola Graholska <vola.graholska@gmail.com>
2019-08-14 00:23:52 +0300

    feat: add socket connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import setupSocket from './socket/socket'
+import reducers from './reducers/reducer'
+import handleNewMessage from './sagas/sagas'
 
-import chat from './reducers'
+const username = 'boba'
 
-const store = createStore(chat)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+    reducers,
+    applyMiddleware(sagaMiddleware)
+    )
+
+const socket = setupSocket(store.dispatch, username)
+sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
     <Provider store={store}>
@@ -16,7 +27,5 @@ ReactDOM.render(
     </Provider>, 
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
